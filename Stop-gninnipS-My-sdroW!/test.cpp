2d807ce320ea073f69c684410ef6be3a2a170b54
Sandrino <s.becirbegovic@gmail.com>
2021-05-24 01:33:34 +0200

    cpp

---
+++ Stop-gninnipS-My-sdroW!/test.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+/* #include <boost/algorithm/string/split.hpp> */
+/* #include <boost/algorithm/string/classification.hpp> // Include boost::for is_any_of */
+
+
+using namespace std;
+
+
+std::string spinWords(const std::string &str)
+{
+    std::string res;
+    stringstream ss(str);
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> words(begin, end);
+    std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+
+    /* std::vector<std::string> words; */
+    /* boost::split(words, str, boost::is_any_of(", "), boost::token_compress_on); */
+
+    for(string& word : words){
+        if (word.size() >= 5){
+            reverse(word.begin(), word.end());
+        }
+        res += word + " ";
+    }
+
+
+
+    return res.substr(0, res.size()-1);
+}// spinWords
+
+
+int main(){
+
+    std::string res;
+    res = spinWords("Hey fellow warriors");
+    cout << res << endl;
+    
+}
