3e879a44e2054d5af138a93c2b3fef24af37e1d0
Sandrino <s.becirbegovic@gmail.com>
2021-05-24 00:52:09 +0200

    cpp

---
+++ simple-pig-latin/test.cpp
@@ -0,0 +1,53 @@
+#include <cctype>
+#include <string>
+#include <iostream>
+
+#define START 0
+
+
+using namespace std;
+std::string pig_it(std::string str)
+{
+
+    std::string word;
+    std::string res;
+    int pos;
+    int i=0;
+    while (true){
+        pos = str.find(" ");
+        if (pos != std::string::npos){
+            word = str.substr(START, pos);
+            str = str.substr(pos+1);
+            if (word.size() == 1 && !isalpha(word[0])){
+                res += word + " ";
+                word = "";
+                continue;
+            }
+            cout << "Before word: " << word << endl;
+            word = word.substr(1) + word[0] + "ay ";
+            res += word;
+            word = "";
+        }
+        else{
+            if(!isalpha(str[str.size()-1])){
+                res += str[str.size()-1];
+                break;
+            }
+            res += str.substr(1) + str[0] + "ay";
+            break;
+        }
+    }
+        
+    return res;
+}
+
+
+int main(){
+
+
+    /* std::string res = pig_it("This is my string"); */
+    std::string res = pig_it(". Sandi Hier da !");
+    std::cout << res << std::endl;
+    
+    return 0;
+}
