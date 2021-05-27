19262e31a9feb29bc646fd111a13239375ac4ff1
Sandrino <s.becirbegovic@gmail.com>
2021-05-27 23:51:20 +0200

    Added Description

--- BuildTowers/test.cpp
+++ BuildTowers/test.cpp
@@ -1,4 +1,20 @@
+//
 // chalange: https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/cpp
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+//
+//
+//
+// Tower with 6 floors:[
+// '     *     ', 
+// '    ***    ', 
+// '   *****   ', 
+// '  *******  ', 
+// ' ********* ', 
+// '***********'
+// ]
+
+
 #include <cstddef>
 #include <iostream>
 #include <string>
